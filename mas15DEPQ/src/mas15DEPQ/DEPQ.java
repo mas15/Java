972944a127a6bb0c93885307b2793d5d39376f47
Mateusz Stankiewicz <mas15@aber.ac.uk>
2016-01-30 21:30:43 +0100

    Update DEPQ.java

--- mas15DEPQ/src/mas15DEPQ/DEPQ.java
+++ mas15DEPQ/src/mas15DEPQ/DEPQ.java
@@ -1,55 +1,18 @@
 package mas15DEPQ;
 
-
-
-/**
- * A simple Double Ended Priority Queue Interface
- * @author bpt
- */
 public interface DEPQ {
-  /**
-   * Returns the smallest element in the DEPQ but does not remove it from the DEPQ
-   * @return returns the smallest element in the DEPQ
- * @throws QueueEmptyException 
-   */
+  
   public Comparable inspectLeast();
   
-  /**
-   * Returns the largest element in the DEPQ but does not remove it from the DEPQ
-   * @return returns the largest element in the DEPQ
- * @throws QueueEmptyException 
-   */
   public Comparable inspectMost();
 
- /**
-  * Adds an element to the DEPQ
-  * @param c the element to insert into the DEPQ
-  */
   public void add(Comparable c);
  
-  /**
-   * Removes the smallest element from the DEPQ and returns it
-   * @return returns the smallest element in the DEPQ
- * @throws QueueEmptyException 
-   */
   public Comparable getLeast();  
     
-  
-  /**
-   * Removes the largest element from the DEPQ and returns it
-   * @return returns the largest element in the DEPQ
-   */
   public Comparable getMost();
  
-  /**
-   * Checks if the DEPQ is empty
-   * @return returns true if the queue is empty
-   */
   public boolean isEmpty();
   
-  /**
-   * Returns the size of the DEPQ
-   * @return returns the number of elements currently in the DEPQ
-   */
   public int size();
 }
